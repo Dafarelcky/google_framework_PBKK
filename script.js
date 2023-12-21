c7f73d0dcb152c9288b5f79e63f9495027539532
Dafarelcky <90192132+Dafarelcky@users.noreply.github.com>
2023-12-21 14:48:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ form.addEventListener("submit", async (e) => {
 
   try {
     const response = await fetch("https://script.google.com/macros/s/AKfycbx-hwhvSl6Qcp580iClMwv-DKVbqqlc00ApVA569AzO0c18icajsJYWBhUXUq626F48/exec", {
+      redirect: "follow",
       method: "POST",
       body: new FormData(form),
     });
