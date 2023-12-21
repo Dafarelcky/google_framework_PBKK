ee0ec254b79c603c30b5beda510e6172de03821e
Dafarelcky <90192132+Dafarelcky@users.noreply.github.com>
2023-12-21 14:30:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// script.js
+
+const form = document.getElementById("registrationForm");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbx-hwhvSl6Qcp580iClMwv-DKVbqqlc00ApVA569AzO0c18icajsJYWBhUXUq626F48/exec", {
+      method: "POST",
+      body: new FormData(form),
+    });
+
+    const data = await response.json();
+
+    if (data.result === "success") {
+      alert("Registrasi berhasil. Terima kasih sudah mendaftar.");
+      window.location.reload();
+    } else {
+      throw new Error(data.error);
+    }
+  } catch (error) {
+    console.error("Error! Something's wrong", error.message);
+    alert("Registrasi Gagal!! Silahkan coba lagi.");
+  }
+});
